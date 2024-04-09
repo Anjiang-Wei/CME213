47704a72ace77e2f521e3a5c55a33b9da4ed6a0b
Anjiang-Wei <1020681930@qq.com>
2024-04-08 22:41:31 -0700

    q2

--- hw1/starter-code/main_q2.cpp
+++ hw1/starter-code/main_q2.cpp
@@ -25,3 +25,81 @@ broadcasting. Say you are testing the result C = A * B, test with:
 4. A of shape (1, 1), B of shape (m != 1, n != 1)
 Please test any more cases that you can think of.
 */
+
+
+TEST(testMatrix2D, test1) {
+  Matrix2D<int> a(2, 2);
+  a(0, 0) = 1;
+  a(0, 1) = 2;
+  a(1, 0) = 3;
+  a(1, 1) = 4;
+  Matrix2D<int> b(a);
+  Matrix2D<int> c = a.dot(b);
+  ASSERT_EQ(c(0,0), 1)
+      << "This does not fail, hence this message is not printed.";
+  ASSERT_EQ(c(0,1), 4)
+      << "This does not fail, hence this message is not printed.";
+  ASSERT_EQ(c(1,0), 9)
+      << "This does not fail, hence this message is not printed.";
+  ASSERT_EQ(c(1,1), 16)
+      << "This does not fail, hence this message is not printed.";
+}
+
+TEST(testMatrix2D, test2) {
+  Matrix2D<int> a(1, 2);
+  a(0, 0) = 1;
+  a(0, 1) = 2;
+  Matrix2D<int> b(2, 2);
+  b(0, 0) = 1;
+  b(0, 1) = 2;
+  b(1, 0) = 3;
+  b(1, 1) = 4;
+  Matrix2D<int> c = a.dot(b);
+  ASSERT_EQ(c(0,0), 1)
+      << "This does not fail, hence this message is not printed.";
+  ASSERT_EQ(c(0,1), 4)
+      << "This does not fail, hence this message is not printed.";
+  ASSERT_EQ(c(1,0), 3)
+      << "This does not fail, hence this message is not printed.";
+  ASSERT_EQ(c(1,1), 8)
+      << "This does not fail, hence this message is not printed.";
+}
+
+TEST(testMatrix2D, test3) {
+  Matrix2D<int> a(2, 2);
+  a(0, 0) = 1;
+  a(0, 1) = 2;
+  a(1, 0) = 3;
+  a(1, 1) = 4;
+  Matrix2D<int> b(2, 1);
+  b(0, 0) = 1;
+  b(1, 0) = 2;
+  Matrix2D<int> c = a.dot(b);
+  ASSERT_EQ(c(0,0), 1)
+      << "This does not fail, hence this message is not printed.";
+  ASSERT_EQ(c(0,1), 2)
+      << "This does not fail, hence this message is not printed.";
+  ASSERT_EQ(c(1,0), 6)
+      << "This does not fail, hence this message is not printed.";
+  ASSERT_EQ(c(1,1), 8)
+      << "This does not fail, hence this message is not printed.";
+}
+
+TEST(testMatrix2D, test4) {
+  Matrix2D<int> a(1, 1);
+  a(0, 0) = 2;
+  Matrix2D<int> b(2, 2);
+  b(0, 0) = 1;
+  b(0, 1) = 2;
+  b(1, 0) = 3;
+  b(1, 1) = 4;
+  Matrix2D<int> c = a.dot(b);
+  ASSERT_EQ(c(0,0), 2)
+      << "This does not fail, hence this message is not printed.";
+  ASSERT_EQ(c(0,1), 4)
+      << "This does not fail, hence this message is not printed.";
+  ASSERT_EQ(c(1,0), 6)
+      << "This does not fail, hence this message is not printed.";
+  ASSERT_EQ(c(1,1), 8)
+      << "This does not fail, hence this message is not printed.";
+}
--- hw1/starter-code/matrix_rect.hpp
+++ hw1/starter-code/matrix_rect.hpp
@@ -14,6 +14,9 @@ template <typename T>
 bool Broadcastable(Matrix2D<T>& A, Matrix2D<T>& B) {
   // TODO: Write a function that returns true if either of the matrices can be
   // broadcast to be compatible with the other for elementwise multiplication.
+  bool row_broadcastable = (A.size_rows() == 1) || (B.size_rows() == 1) || (A.size_rows() == B.size_rows());
+  bool col_broadcastable = (A.size_cols() == 1) || (B.size_cols() == 1) || (A.size_cols() == B.size_cols());
+  return row_broadcastable && col_broadcastable;
 }
 
 template <typename T>
@@ -30,27 +33,47 @@ class Matrix2D {
  public:
   // Empty matrix
   Matrix2D() { 
-    // TODO
+    n_rows = 0;
+    n_cols = 0;
   }
 
   // Constructor takes argument (m,n) = matrix dimension.
   Matrix2D(const int m, const int n) {
       // TODO: Hint: allocate memory for m * n elements using keyword 'new'
+      data_ = new T[m * n];
+      n_rows = m;
+      n_cols = n;
   }
 
   // Destructor
   ~Matrix2D() {
     // TODO: Hint: Use keyword 'delete'
+    n_rows = 0;
+    n_cols = 0;
+    delete[] data_;
   }
 
   // Copy constructor
   Matrix2D(const Matrix2D& other) : n_rows(other.n_rows), n_cols(other.n_cols) {
     // TODO
+    // Allocate new memory for data_
+    data_ = new T[n_rows * n_cols];
+
+    // Copy the elements from the other matrix
+    for (unsigned int i = 0; i < n_rows * n_cols; ++i) {
+        data_[i] = other.data_[i];
+    }
   }
 
   // Copy assignment operator
   Matrix2D& operator=(const Matrix2D& other) {
     // TODO
+    n_rows = other.n_rows;
+    n_cols = other.n_cols;
+    data_ = new T[n_rows * n_cols];
+    for (unsigned int i = 0; i < n_rows * n_cols; ++i) {
+        data_[i] = other.data_[i];
+    }
     return *this;
   }
 
@@ -58,33 +81,45 @@ class Matrix2D {
   Matrix2D(Matrix2D&& other) noexcept 
     : n_rows(other.n_rows), n_cols(other.n_cols), data_(other.data_) {
     // TODO
+    other.data_ = nullptr;
+    other.n_rows = 0;
+    other.n_cols = 0;
   }
 
   // Move assignment operator
   Matrix2D& operator=(Matrix2D&& other) noexcept {
     // TODO
+    n_rows = other.n_rows;
+    n_cols = other.n_cols;
+    data_ = other.data_;
     return *this;
   }
 
-  unsigned int size_rows() const { return 0; } // TODO
-  unsigned int size_cols() const { return 0; } // TODO
+  unsigned int size_rows() const { return n_rows; } // TODO
+  unsigned int size_cols() const { return n_cols; } // TODO
 
   // Returns reference to matrix element (i, j).
   T& operator()(int i, int j) {
     // TODO: Hint: Element (i,j) for 0 <= i < n_rows and 0 <= j < n_cols 
     // is stored at data[i * n_cols + j]. 
-    return data_[0];
+    return data_[i * n_cols + j];
   }
     
   void Print(std::ostream& ostream) {
     // TODO
+    for (int i = 0; i < n_rows; i++) {
+      for (int j = 0; j < n_cols; j++) {
+        ostream << (*this)(i, j) << " ";
+      }
+      ostream << std::endl;
+    }
   }
 
   Matrix2D<T> dot(Matrix2D<T>& mat) {
     if (n_rows == mat.size_rows() && n_cols == mat.size_cols()) {
       Matrix2D<T> ret(n_rows, n_cols);
-      for (int i = 0; i < n_rows; i++) {
-        for (int j = 0; j < n_cols; j++) {
+      for (size_t i = 0; i < n_rows; i++) {
+        for (size_t j = 0; j < n_cols; j++) {
           ret(i, j) = (*this)(i, j) * mat(i, j);
         }
       }
@@ -93,7 +128,20 @@ class Matrix2D {
       // TODO: Replace the code in this scope.
       // Compute and return the elementwise product of the two Matrix2D's
       // "*this" and "mat" after appropriate broadcasting.
-      Matrix2D<T> ret;
+      unsigned int ret_row = std::max(n_rows, mat.size_rows());
+      unsigned int ret_col = std::max(n_cols, mat.size_cols());
+      Matrix2D<T> ret(ret_row, ret_col);
+      for (unsigned int i = 0; i < ret_row; i++) {
+        for (unsigned int j = 0; j < ret_col; j++) {
+          {
+            int this_i = std::min(i, n_rows - 1);
+            int this_j = std::min(j, n_cols - 1);
+            int mat_i = std::min(i, mat.size_rows() - 1);
+            int mat_j = std::min(j, mat.size_cols() - 1);
+            ret(i, j) = (*this)(this_i, this_j) * mat(mat_i, mat_j);
+          }
+        }
+      }
 
       return ret;
     } else {
@@ -108,6 +156,7 @@ class Matrix2D {
 template <typename T>
 std::ostream& operator<<(std::ostream& stream, Matrix2D<T>& m) {
   // TODO
+  m.Print(stream);
   return stream;
 }
 
